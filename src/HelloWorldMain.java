5300909caa749d8e156b7f1f4f21570ddcbca197
wolfgang <wolfgang@linux-nhel.fritz.box>
2016-12-06 13:13:18 +0100

    asdfasf

---
+++ src/HelloWorldMain.java
@@ -0,0 +1,14 @@
+
+public class HelloWorldMain {
+
+    public HelloWorldMain() {
+        System.out.println("hELLO wORLD");
+    }
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
